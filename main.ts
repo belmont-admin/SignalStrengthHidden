5050031df494b23062927360b0d56570c2d3bcbe
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-27 08:58:32 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+radio.setGroup(255)
+let instance = 0
+let channelList = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+let channels = [1, 2, 0, 0]
+for (let index = 0; index < 4; index++) {
+    channels.push(channelList.removeAt(Math.randomRange(0, channelList.length - 1)))
+}
