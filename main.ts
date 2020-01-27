a670a21653f8bf4c0dde5b1ebb95ace1c06d82fb
belmont-admin <58621487+belmont-admin@users.noreply.github.com>
2020-01-27 15:03:51 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,13 +3,38 @@ radio.onReceivedString(function (receivedString) {
     radio.sendString("ping")
 })
 input.onButtonPressed(Button.AB, function () {
-	
+    myNumber += 1
+    basic.showNumber(myNumber)
+    if (myNumber == 1) {
+        radio.sendString(channelsText)
+        radio.setGroup(1)
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(nextChannel)
+    basic.pause(1000)
+    basic.showNumber(myNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    basic.showString(channelsText)
+    basic.pause(1000)
+    basic.showNumber(myNumber)
+})
+let nextChannel = 0
+let channelsText = ""
+let myNumber = 0
+basic.showString("H")
+myNumber = 0
 let instance = 0
 radio.setGroup(255)
 radio.setTransmitPower(7)
 let PossibleChannels = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-let channels = [1, 2, 0, 0]
+let channels: number[] = []
+channelsText = ""
 for (let index = 0; index < 4; index++) {
     channels.push(PossibleChannels.removeAt(Math.randomRange(0, PossibleChannels.length - 1)))
 }
+for (let index = 0; index <= 3; index++) {
+    channelsText = "" + channelsText + convertToText(channels[index])
+}
+nextChannel = channels[1]
